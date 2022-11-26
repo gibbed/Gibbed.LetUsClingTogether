3530ed0f6da29f763f32ebb0706f089040c5775e
Rick Gibbed <rick@gibbed.us>
2022-11-26 06:19:43 -0600

    Clean up bogocrypt code.

--- projects/Gibbed.Reborn.FileFormats/BaseBogoCrypt.cs
+++ projects/Gibbed.Reborn.FileFormats/BaseBogoCrypt.cs
@@ -24,36 +24,6 @@ namespace Gibbed.Reborn.FileFormats
 {
     public abstract class BaseBogoCrypt
     {
-        protected static readonly byte[] Table2;
-        protected static readonly byte[] Table3;
-
-        static BaseBogoCrypt()
-        {
-            Table2 = new byte[]
-            {
-                0xB0, 0xBC, 0x6D, 0x19, 0xE5, 0x14, 0xB6, 0xEA,
-                0xF4, 0xCB, 0x16, 0xB1, 0x7D, 0xE5, 0x7F, 0xB5,
-                0x0B, 0x7C, 0x5E, 0xC4, 0x2D, 0x4F, 0x31, 0x99,
-                0x67, 0x98, 0x19, 0xE8, 0x28, 0x58, 0xDE, 0xC9,
-                0xC1, 0x9B, 0xC9, 0x83, 0x34, 0xC4, 0x64, 0x85,
-                0x36, 0xFA, 0x0E, 0xDE, 0xFB, 0xE7, 0x68, 0x99,
-                0x71, 0x32, 0x34, 0x36, 0xE5, 0x01, 0x7D, 0x63,
-                0x5C, 0x81, 0x77, 0x7C,
-            };
-            Table3 = new byte[]
-            {
-                0xC6, 0x53, 0x98, 0xED, 0xD0, 0xAA, 0xE0, 0x1B,
-                0x43, 0x31, 0x16, 0x32, 0xCE, 0x0D, 0x52, 0x57,
-                0x7B, 0x72, 0x0F, 0xFB, 0xB7, 0xA9, 0xC6, 0xF1,
-                0xF1, 0x72, 0x2C, 0x89, 0x5F, 0xB2, 0x1B, 0xFA,
-                0xA8, 0xDB, 0xE3, 0xE2, 0x41, 0xB5, 0xBE, 0xC9,
-                0x5D, 0xAE, 0xC6, 0x21, 0xE4, 0xD2, 0x88, 0x74,
-                0x97, 0x26, 0x4F, 0x87, 0x7A, 0xF0, 0x69, 0x7A,
-                0xB7, 0x7E, 0x15, 0x09, 0xBA, 0x55, 0xAD, 0xCA,
-                0xB1, 0x60, 0x34, 0x76,
-            };
-        }
-
         protected static void Enshift(byte[] bytes, int offset, int count, int shift)
         {
             if (count <= 0)
@@ -86,7 +56,7 @@ namespace Gibbed.Reborn.FileFormats
             bytes[o] = (byte)((bytes[o] << shift) | (firstByte >> rightShift));
         }
 
-        protected static void XorWithTable(byte[] bytes, int offset, int count, int seed, byte[] table)
+        protected static void XorWithTable(byte[] bytes, int offset, int count, byte[] table, int seed)
         {
             if (count <= 0)
             {
@@ -103,6 +73,26 @@ namespace Gibbed.Reborn.FileFormats
                 bytes[o + 1] ^= table[tableOffset + 1];
                 bytes[o + 2] ^= table[tableOffset + 2];
                 bytes[o + 3] ^= table[tableOffset + 3];
+            }
+        }
+
+        protected static void XorWithTableReverse(byte[] bytes, int offset, int count, byte[] table, int seed)
+        {
+            if (count <= 0)
+            {
+                return;
+            }
+            int tableLength = table.Length >> 2;
+            int tableIndex = seed;
+            for (int i = 0, o = offset; i + 4 <= count; i += 4, o += 4)
+            {
+                tableIndex %= tableLength;
+                var tableOffset = tableIndex * 4;
+                tableIndex++;
+                bytes[o + 0] ^= table[tableOffset + 3];
+                bytes[o + 1] ^= table[tableOffset + 2];
+                bytes[o + 2] ^= table[tableOffset + 1];
+                bytes[o + 3] ^= table[tableOffset + 0];
             }
         }
     }
